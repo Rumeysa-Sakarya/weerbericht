412ac7a8a9f932c8e528b6ca11d7fabda05a8b3b
Rumeysa-sakarya <rumeysakarya.10@gmail.com>
2025-02-14 02:05:57 +0100

    added the icon via api

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ function refreshWeather(response) {
   let speedElement = document.querySelector("#speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let icon = document.querySelector("#icon");
 
   console.log(response.data);
 
@@ -15,6 +16,10 @@ function refreshWeather(response) {
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   speedElement.innerHTML = `${response.data.wind.speed} km/h`;
   timeElement.innerHTML = formatDate(date);
+  icon.innerHTML = `<img
+                src="${response.data.condition.icon_url}"
+                class="weather-app-icon"
+              />`;
   temperatureElement.innerHTML = Math.round(temperature);
 }
 
