62ec3ee01c2c426b4ac4f8f4e263ef2b38275f1b
Rumeysa-sakarya <rumeysakarya.10@gmail.com>
2025-02-14 03:17:09 +0100

    added the forecast in html & css

--- src/index.js
+++ src/index.js
@@ -9,8 +9,6 @@ function refreshWeather(response) {
   let date = new Date(response.data.time * 1000);
   let icon = document.querySelector("#icon");
 
-  console.log(response.data);
-
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
